5067894180662b0cc9f704b386fdecbe63e86509
Marcos Pablo <marcos.almeida.tw@naturapay.net>
2025-03-07 19:26:51 -0300

    adding static dir and copy dir function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 from textnode import TextNode, TextType
+import os, shutil
+
+def copy_from_static_to_public():
+    def del_dir(path: str):
+        if os.path.isfile(path):
+            pass
+        else:
+            pass
+
+    def copy_dir(source_path: str, destination_path: str):
+        items = os.listdir(source_path)
+        for item in items:
+            if os.path.isfile(f"{source_path}/{item}"):
+                shutil.copy(f"{source_path}/{item}", destination_path)
+            else:
+                os.mkdir(f"{destination_path}/{item}")
+                copy_dir(f"{source_path}/{item}", f"{destination_path}/{item}")
+
+    copy_dir("./static/", "./public/")
+
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(text_node)
+    copy_from_static_to_public()
 
 main()
