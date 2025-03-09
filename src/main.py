516d10b0f87798a5861752bb6d184ca416ff3c4e
Marcos Pablo <marcos.almeida.tw@naturapay.net>
2025-03-09 18:41:25 -0300

    Generating pages recursively

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from copy_static import copy_from_static_to_public
-from get_content import generate_page
+from get_content import generate_page, generate_pages_recursive
 
 def main():
     copy_from_static_to_public()
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    # generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
 
 main()
