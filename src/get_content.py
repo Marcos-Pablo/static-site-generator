0944ab735b9c29a86f03aa8ce4ffa5e32882ab29
Marcos Pablo <marquin.pablo@gmail.com>
2025-03-09 18:41:25 -0300

    Generating pages recursively

--- src/get_content.py
+++ src/get_content.py
@@ -1,6 +1,46 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    template_file = open(template_path)
+    template = template_file.read()
+    template_file.close()
+
+    items = os.listdir(dir_path_content)
+    for item in items:
+        if os.path.isfile(f"{dir_path_content}/{item}"):
+            file_name = None
+            file_extension = None
+
+            for i in range(len(item) - 1, -1, -1):
+                if item[i] == ".":
+                    file_name = item[:i]
+                    file_extension = item[i + 1:]
+                    break
+            
+            if file_extension != "md":
+                print("Ignoring non markdown file: ", item)
+                continue
+
+            markdown_file = open(f"{dir_path_content}/{item}")
+            markdown = markdown_file.read()
+            markdown_file.close()
+
+            html_node = markdown_to_html_node(markdown)
+            html = html_node.to_html()
+            title = extract_title(markdown)
+            
+            template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+            file = open(f"{dest_dir_path}/{file_name}.html", "w")
+            file.write(template)
+            file.close()
+        else:
+            generate_pages_recursive(f"{dir_path_content}/{item}", template_path, f"{dest_dir_path}/{item}")
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_file = open(from_path)
