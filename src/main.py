7568845b48c9ad0b7d4367a4ccd5b42b981c65e1
Marcos Pablo <marcos.almeida.tw@naturapay.net>
2025-03-09 19:04:21 -0300

    Generating pages recursively refactor

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from copy_static import copy_from_static_to_public
-from get_content import generate_page, generate_pages_recursive
+from get_content import generate_pages_recursive
 
 def main():
     copy_from_static_to_public()
-    # generate_page("./content/index.md", "./template.html", "./public/index.html")
     generate_pages_recursive("./content", "./template.html", "./public")
 
 main()
