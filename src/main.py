da756ccdaf71d244a67f43074f7406a489d6199a
Marcos Pablo <marcos.almeida.tw@naturapay.net>
2025-03-09 21:50:13 -0300

    Refactoring copy from static to public function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from copy_static import copy_from_static_to_public
+from copy_dir import copy_dir
 from get_content import generate_pages_recursive
+import os, shutil
+
+static_dir_path = "./static"
+content_dir_path = "./content"
+public_dir_path = "./public"
+template_path = "./template.html"
 
 def main():
-    copy_from_static_to_public()
-    generate_pages_recursive("./content", "./template.html", "./public")
+    if os.path.exists(public_dir_path):
+        shutil.rmtree(public_dir_path)
+
+    copy_dir(static_dir_path, public_dir_path)
+    generate_pages_recursive(content_dir_path, template_path, public_dir_path)
 
 main()
