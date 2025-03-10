336473d9fc1b0b3987f72c5c97ac2f6c9b0c2785
Marcos Pablo <marcos.almeida.tw@naturapay.net>
2025-03-10 20:29:56 -0300

    Refactoring application structure in order to use git hub pages

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from copy_dir import copy_dir
 from get_content import generate_pages_recursive
-import os, shutil
+import os, shutil, sys
 
 static_dir_path = "./static"
 content_dir_path = "./content"
-public_dir_path = "./public"
+public_dir_path = "./docs"
 template_path = "./template.html"
+base_path = sys.argv[1] if len(sys.argv) >= 2 else "/"
 
 def main():
+    print(base_path)
     if os.path.exists(public_dir_path):
         shutil.rmtree(public_dir_path)
 
     copy_dir(static_dir_path, public_dir_path)
-    generate_pages_recursive(content_dir_path, template_path, public_dir_path)
+    generate_pages_recursive(content_dir_path, template_path, public_dir_path, base_path)
 
 main()
